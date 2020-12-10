4d0874fe3f7aab5d99f5f19279e4bbc38c12c22e
Trung Vu <vutientrung.danang@gmail.com>
2020-12-10 13:57:11 +0700

    Update

--- Reboot.py
+++ Reboot.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome(executable_path=r"D:\PySe\chromedriver.exe")
-browser.get("http://192.168.81.11/")
+browser = webdriver.Chrome(executable_path=r"D:\PySe\chromedriver.exe") #location of chrome webdriver
+browser.get("http://1A.B.C.D/") # URL of finger print devices
 browser.set_page_load_timeout(5)
-browser.find_element_by_xpath('/html/body/center/table[2]/tbody/tr[2]/td[3]/input').send_keys("282")
-browser.find_element_by_xpath('/html/body/center/table[2]/tbody/tr[3]/td[3]/input').send_keys("1207")
-browser.find_element_by_xpath('/html/body/center/table[2]/tbody/tr[5]/td/input[1]').click()
-browser.get("http://192.168.81.11/form/Device")
+browser.find_element_by_xpath('/html/body/center/table[2]/tbody/tr[2]/td[3]/input').send_keys("282") #Enter username
+browser.find_element_by_xpath('/html/body/center/table[2]/tbody/tr[3]/td[3]/input').send_keys("1207") #Enter password
+browser.find_element_by_xpath('/html/body/center/table[2]/tbody/tr[5]/td/input[1]').click() #click login
+browser.get("http://A.B.C.D/form/Device") #Goto URL of reboot.
